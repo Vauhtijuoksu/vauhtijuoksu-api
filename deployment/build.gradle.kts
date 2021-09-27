b5465f580fa3bfea83278c4b78682807c078858c
Antti Möttönen <antti.moettoenen@gmail.com>
2021-09-27 20:20:48 +0300

    Add linting for helm charts

--- deployment/build.gradle.kts
+++ deployment/build.gradle.kts
@@ -1,3 +1,38 @@
+import java.io.FileOutputStream
+
 plugins {
     id("vauhtijuoksu-api.kotlin-common-conventions")
+}
+
+tasks {
+    val lintCharts by registering {
+        fun lintDir(chartDirectory: String, output: String) {
+            val res = exec {
+                commandLine("helm", "lint", chartDirectory)
+                standardOutput = FileOutputStream(output, true)
+                isIgnoreExitValue = true
+            }
+            if (res.exitValue != 0) {
+                throw GradleException("Helm lint failed. Check $output for details")
+            }
+        }
+
+        description = "Lint helm charts"
+        group = "Verification"
+        inputs.dir("$projectDir/api-server")
+        inputs.dir("$projectDir/mockserver")
+
+        val mockserverOutput = "$buildDir/mockserver-lint-results.txt"
+        outputs.file(mockserverOutput)
+        val apiServerOutput = "$buildDir/api-server-lint-results.txt"
+        outputs.file(apiServerOutput)
+        doLast {
+            lintDir("$projectDir/mockserver", mockserverOutput)
+            lintDir("$projectDir/api-server", apiServerOutput)
+        }
+    }
+
+    check {
+        finalizedBy(lintCharts)
+    }
 }
