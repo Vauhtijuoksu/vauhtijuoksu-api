9a99f03d19a591a704ecf9d24f1108a4e9d9833c
Antti Möttönen <antti.moettoenen@gmail.com>
2021-10-07 13:20:34 +0300

    Add model for GameData

---
+++ models/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("vauhtijuoksu-api.kotlin-common-conventions")
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-annotations")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("io.vertx:vertx-core")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
